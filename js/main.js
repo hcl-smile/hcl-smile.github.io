64ec53070e09bbf17a2ed69f05f2c49b0e9277db
hcl-smile <13215027210@163.com>
2019-08-29 10:46:56 +0800

    修复效果

--- js/main.js
+++ js/main.js
@@ -92,7 +92,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "4e210986ed56f47eeeb2";
+/******/ 	var hotCurrentHash = "7afc5491e4882bc6246c";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
